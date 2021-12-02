e0d3e827bd4585c8da6244ec683420a67d543108
andrewmchoward <91852627+andrewmchoward@users.noreply.github.com>
2021-12-01 21:57:33 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+    
+    
+    
+$(window).resize(function(){
+    if ($(window).width() <= 900) {
+        $("body").css("text-align", "center");
+    }
+    else {
+        $("body").css("text-align", "left");
+        $("footer").css("text-align", "center");
+    }
+})
+
+
+    
